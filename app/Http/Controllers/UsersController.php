104e3246c72870d515bd89417e5de8478961d991
山泉农夫 <1942480291@qq.com>
2018-12-26 10:22:56 +0000

    Only authorized user can edit user profile

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,6 +8,11 @@ use App\Http\Requests\UserRequest;
 use App\Handlers\ImageUploadHandler;
 class UsersController extends Controller
 {
+	//身份验证（Auth）中间件来过滤未登录用户的 edit, update 动作
+	public function __construct(){
+		$this->middleware('auth',['except'=>['show']]);
+	}
+
     //个人页面的展示
     public function show(User $user){
 
@@ -16,12 +21,18 @@ class UsersController extends Controller
 
     //编辑表单
     public function edit(User $user){
+    	//检验用户是否授权
+    	$this->authorize('update',$user);
+    	
     	return view('users.edit',compact('user'));
     }
 
     //对编辑表单提交的数据进行处理
     public function update(UserRequest $request,ImageUploadHandler $uploader,User $user){
     	//dd($request->avatar);
+    	//检验用户是否授权
+    	$this->authorize('update', $user);
+
     	$data = $request->all();
     	if($request->avatar){
     		$result = $uploader->save($request->avatar,'avatars',$user->id,362);
