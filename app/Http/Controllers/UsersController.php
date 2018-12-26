1365caf3d6fadc44d61de96d4fb2c7b536ad4ae3
山泉农夫 <1942480291@qq.com>
2018-12-26 08:38:57 +0000

    Basic user profile edit

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,11 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Http\Requests\UserRequest;
 class UsersController extends Controller
 {
     //个人页面的展示
     public function show(User $user){
 
     	return view('users.show',compact('user'));
+    }
+
+    //编辑表单
+    public function edit(User $user){
+    	return view('users.edit',compact('user'));
+    }
+
+    //对编辑表单提交的数据进行处理
+    public function update(UserRequest $request,User $user){
+    	$user->update($request->all());
+
+    	return redirect()->route('users.show',$user->id)->with('success','个人资料更新成功');
     }
 }
