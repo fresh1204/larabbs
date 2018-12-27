49eac8007c6afd1047903fad8d2ed93cab486864
山泉农夫 <1942480291@qq.com>
2018-12-27 06:25:13 +0000

    Topic order

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -14,11 +14,14 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index()
+	public function index(Request $request,Topic $topic)
 	{
 		//$topics = Topic::paginate(30);
 		//方法 with() 提前加载了我们后面需要用到的关联属性 user 和 category，并做了缓存
-		$topics = Topic::with('user','category')->paginate();
+		//$topics = Topic::with('user','category')->paginate();
+
+		//$request->order 是获取 URI http://larabbs.test/topics?order=recent 中的 order 参数
+		$topics = $topic->withOrder($request->order)->paginate(20);
 		return view('topics.index', compact('topics'));
 	}
 
