c733780b24a2d8627924594da04634e19344d933
山泉农夫 <1942480291@qq.com>
2018-12-28 02:39:15 +0000

    Prevent XSS injection

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -22,6 +22,9 @@ class TopicObserver
     //在 Topic 模型保存时触发的 saving 事件
     public function saving(Topic $topic)
     {
+        //在数据入库前对话题内容进行过滤
+        $topic->body = clean($topic->body, 'user_topic_body');
+
     	$topic->excerpt = make_excerpt($topic->body);
     }
 }
